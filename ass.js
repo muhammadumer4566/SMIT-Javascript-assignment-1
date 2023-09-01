055c4bff0829aac2cdf03c68eb119bdbea35d40d
muhammadumer4566 <muhammadumerfayyaz.2002@gmail.com>
2023-09-01 19:37:37 +0500

    JS

---
+++ ass.js
@@ -0,0 +1,60 @@
+                    //   ! Chapter 01 JS Assignment
+
+// ? Q1)
+// alert("Assalamualikum");
+
+// ? Q2)
+// alert("Error! Please enter a valid password");
+
+// ? Q3)
+// alert("Welcome to JS Land.... \n Happy Coding!");
+
+// ? Q4)
+// alert("Welcome to JS Land....");
+// alert("Happy Coding! \n prevant this page from creating additional dialogs.");
+
+// ? Q5)
+// alert("Hello...I can run JS through my web browser's console");
+// console.log("Hello...I can run JS through my web browser's console");
+
+
+                            //  !  Chapter 02   JS Assignment
+
+    
+//  ? Q1)
+//   var username;
+
+// ? Q2)
+//    var myName="Muhammad Umer"; 
+
+// ? Q3)
+    //  var JS= "Hello World";
+    //  alert(JS);
+
+// ? Q4) 
+    //  var name="umer";
+    //  var age= "20 years old";
+    //  var qualification="Bachlors";
+     
+    //  alert(name);
+    //  alert(age);
+    //  alert(qualification);
+
+// ? Q5) 
+    // var JS="PIZZA \n PIZZ \n PIZ \n PI \n P";
+    //   alert(JS);  
+
+// ? Q6)
+    //   var email="muhammadumerfayyaz.2002@gmail";
+    //   alert("my email address is " + email); 
+    
+// ? Q7)
+    //  var book="A smarter way to learn JavaScript";
+    //  alert("I am trying to learn from the book " + book);  
+ 
+// ? Q8)
+    // document.write(" Yah! I can write HTML content through Javascript");    
+
+// ? Q9)
+    //  var JS="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+    //  alert(JS);
